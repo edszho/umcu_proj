cce4b3049e43e547e1906a681b9b8419edf4a40e
edszho <edszho@umich.edu>
2016-06-01 20:03:00 +0000

    used django models to make sql table

--- umcu/memberinfo/models.py
+++ umcu/memberinfo/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+	username = models.CharField(max_length=20, unique=True)
+	password = models.CharField(max_length=250)
+
+class User(models.Model):
+	username = models.ForeignKey(Account, on_delete=models.CASCADE)
+	firstname = models.CharField(max_length=20)
+	lastname = models.CharField(max_length=20)
+	middlename = models.CharField(max_length=20)
+	ssn = models.CharField(max_length=9)
+	dob = models.DateTimeField()
+	license = models.CharField(max_length=15, null=False)
+	license_state = models.CharField(max_length=12, null=False)
+	occupation = models.CharField(max_length=30)
+	membership = models.CharField(max_length=25)
+	gender = (
+			('MALE', 'Male'),
+			('FEMALE', 'Female'),
+			('OTHER', 'Other'),
+	)
--- umcu/umcu/settings.py
+++ umcu/umcu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'memberinfo.apps.MemberinfoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'umcu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accounts',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '8000'
     }
 }
 
@@ -106,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
