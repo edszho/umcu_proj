d910104f8548ee2d28aa8b41b9409e13471db8e3
edszho <edszho@umich.edu>
2016-06-02 01:30:28 +0000

    added background

--- umcu/memberinfo/models.py
+++ umcu/memberinfo/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Account(models.Model):
 	username = models.CharField(max_length=20, unique=True)
 	password = models.CharField(max_length=250)
+	def __str__(self):
+		return self.username
 
 class User(models.Model):
 	username = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -24,3 +26,5 @@ class User(models.Model):
 			('FEMALE', 'Female'),
 			('OTHER', 'Other'),
 	)
+	def __str__(self):
+		return '%s %s %s' % (self.firstname, self.lastname, self.license)
